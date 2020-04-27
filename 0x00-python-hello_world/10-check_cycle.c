d2ecb0580ed220ee3ccd6f233d6039fd0b960307
dairof7 <dairof7@gmail.com>
2020-04-27 14:01:47 -0500

    add # 9

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if is circle list
+ * @list: head of the list
+ * Return: 1 if list is circular and 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	temp = list;
+	while (temp->next != NULL)
+	{
+		if (list == temp->next)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);
+}
