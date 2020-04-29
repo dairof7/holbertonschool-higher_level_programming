606fcee7a2cde2281da4474f94c3b4da29f86814
dairof7 <dairof7@gmail.com>
2020-04-28 21:23:11 -0500

    fix head NULL case

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,14 +12,19 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *current, *new;
 
-	if (head == NULL)
-		return (NULL);
+	
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	
 	new->n = number;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		return (new);
+	}
 	current = *head;
-
 	while (current)
 	{
 		if (number < current->n)
