f27a02d6a91e801581b4fd5426b2ae086ab98310
dairof7 <dairof7@gmail.com>
2020-04-28 21:42:24 -0500

    fix if order

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,13 +10,11 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current, *new;
+	listint_t *current, *new, *_next;
 
-	
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	
 	new->n = number;
 	if (*head == NULL)
 	{
@@ -25,28 +23,29 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 	current = *head;
+	_next = current->next;
 	while (current)
 	{
-		if (number < current->n)
+		if (number <= current->n)
 		{
 			new->next = *head;
 			*head =  new;
 			return (new);
 		}
-		if ((number > current->n) && (number < current->next->n))
-		{
-			new->next = current->next;
-			current->next = new;
-			return (new);
-		}
-		if ((current->next == NULL) && (number > current->n))
+		if ((current->next == NULL) && (number >= current->n))
 		{
 			current->next = new;
 			new->next = NULL;
 			return (new);
 		}
+		if ((number >= current->n) && (number <= _next->n))
+		{
+			new->next = current->next;
+			current->next = new;
+			return (new);
+		}
 		current = current->next;
+		_next = current->next;
 	}
-
 	return (NULL);
 }
