3923eee9e31436ec87fe28207e5cc4b94db9b5eb
dairof7 <dairof7@gmail.com>
2020-04-30 18:24:55 -0500

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,25 +2,26 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    a = sys.argv[1]
-    b = sys.argv[2]
-    c = sys.argv[3]
+
     if (len(sys.argv) != 4):
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
     else:
+        a = int(sys.argv[1])
+        b = sys.argv[2]
+        c = int(sys.argv[3])
         if (sys.argv[2] == "+"):
-            r = add(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(a, b, c, r))
+            r = add(a, c)
+            print("{} {} {} = {:d}".format(a, b, c, r))
         elif (sys.argv[2] == "-"):
-            r = sub(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(a, b, c, r))
+            r = sub(a, c)
+            print("{} {} {} = {:d}".format(a, b, c, r))
         elif (sys.argv[2] == "*"):
-            r = mul(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(a, b, c, r))
+            r = mul(a, c)
+            print("{} {} {} = {:d}".format(a, b, c, r))
         elif (sys.argv[2] == "/"):
-            r = div(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(a, b, c, r))
+            r = div(a, c)
+            print("{} {} {} = {:d}".format(a, b, c, r))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
