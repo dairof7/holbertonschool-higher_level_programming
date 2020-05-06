4e6633bd38c333188a58e29ca20a00e5e2f6716f
dairof7 <dairof7@gmail.com>
2020-05-05 19:56:04 -0500

    add more

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,7 +8,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *list;
 	int i = 0, j = 0, len = 0, palindome = 1;
-	int p[1000];
+	int p[10000000];
 
 	list = *head;
 	if (*head == NULL || list->next == NULL)
