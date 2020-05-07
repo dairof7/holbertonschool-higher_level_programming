7f3c638e44fb53d7b7a3a04dd2087a913354d90b
dairof7 <dairof7@gmail.com>
2020-05-07 16:07:42 -0500

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if (a_dictionary):
         _list = list((sorted(a_dictionary.values())))
     else:
-        return "None"
+        return None
     return _list[-1]
 
 # return (max(a_dictionary.values()) if a_dictionary else None)
