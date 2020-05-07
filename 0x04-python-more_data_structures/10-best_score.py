bd0239f7fffb934f9d9d0092c118e7d21fc08d4b
dairof7 <dairof7@gmail.com>
2020-05-07 17:12:34 -0500

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if (a_dictionary):
-        _list = list((sorted(a_dictionary.values())))
+        list_values = list(a_dictionary.values())
+        list_keys = list(a_dictionary.keys())
+        return list_keys[list_values.index(max(list_values))]
     else:
         return None
-    return _list[-1]
 
-# return (max(a_dictionary.values()) if a_dictionary else None)
+# return (max(a_dictionary, key = a_dictionary.get) if a_dictionary else None)
+# return max(a_dictionary, key=lambda k: a_dictionary[k])
