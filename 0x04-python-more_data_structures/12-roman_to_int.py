a630f04184d19948101c1349d81b762b98a01037
dairof7 <dairof7@gmail.com>
2020-05-07 19:07:52 -0500

    add #12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+def roman_to_int(roman_string):
+    suma = 0
+
+    suma += 1 * roman_string.count("I")
+    suma += 5 * roman_string.count("V")
+    suma += 10 * roman_string.count("X")
+    suma += 50 * roman_string.count("L")
+    suma += 100 * roman_string.count("C")
+    suma += 500 * roman_string.count("D")
+    suma += 1000 * roman_string.count("M")
+
+    if "CM" in roman_string:
+        suma -= 200
+    if "CD" in roman_string:
+        suma -= 200
+    if "XC" in roman_string:
+        suma -= 20
+    if "XL" in roman_string:
+        suma -= 20
+    if "IX" in roman_string:
+        suma -= 2
+    if "IV" in roman_string:
+        suma -= 20
+    return suma
