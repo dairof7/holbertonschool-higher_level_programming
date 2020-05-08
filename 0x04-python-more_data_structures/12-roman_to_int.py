68a2afce7242b2714182ee03bf0ea1cd72a6711f
dairof7 <dairof7@gmail.com>
2020-05-07 19:19:59 -0500

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,26 @@
 def roman_to_int(roman_string):
-    suma = 0
+    if roman_string and type(roman_string) is str:
+        suma = 0
+        suma += 1 * roman_string.count("I")
+        suma += 5 * roman_string.count("V")
+        suma += 10 * roman_string.count("X")
+        suma += 50 * roman_string.count("L")
+        suma += 100 * roman_string.count("C")
+        suma += 500 * roman_string.count("D")
+        suma += 1000 * roman_string.count("M")
 
-    suma += 1 * roman_string.count("I")
-    suma += 5 * roman_string.count("V")
-    suma += 10 * roman_string.count("X")
-    suma += 50 * roman_string.count("L")
-    suma += 100 * roman_string.count("C")
-    suma += 500 * roman_string.count("D")
-    suma += 1000 * roman_string.count("M")
-
-    if "CM" in roman_string:
-        suma -= 200
-    if "CD" in roman_string:
-        suma -= 200
-    if "XC" in roman_string:
-        suma -= 20
-    if "XL" in roman_string:
-        suma -= 20
-    if "IX" in roman_string:
-        suma -= 2
-    if "IV" in roman_string:
-        suma -= 20
-    return suma
+        if "CM" in roman_string:
+            suma -= 200
+        if "CD" in roman_string:
+            suma -= 200
+        if "XC" in roman_string:
+            suma -= 20
+        if "XL" in roman_string:
+            suma -= 20
+        if "IX" in roman_string:
+            suma -= 2
+        if "IV" in roman_string:
+            suma -= 20
+        return suma
+    else:
+        return 0
