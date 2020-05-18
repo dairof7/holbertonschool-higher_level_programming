36ea96206a0a6149e216111f3be9d539844a8a78
dairof7 <dairof7@gmail.com>
2020-05-18 16:28:39 -0500

    adding # 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    r = None
+    try:
+        r = a / b
+        return r
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(r))
