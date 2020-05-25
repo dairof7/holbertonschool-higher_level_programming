11c74f6e0201e0752a8c6b96b3684882eacd1a40
dairof7 <dairof7@gmail.com>
2020-05-25 18:33:21 -0500

    fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -110,8 +110,7 @@ class Rectangle:
     def __del__(self):
         """delete a instance
         """
-        if Rectangle.number_of_instances > 0:
-            Rectangle.number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @staticmethod
