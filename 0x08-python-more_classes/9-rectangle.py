db8bc2bd02dd7dda5d7b35609afe903cd3218001
dairof7 <dairof7@gmail.com>
2020-05-25 18:56:17 -0500

    fix repr

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -105,8 +105,7 @@ class Rectangle:
         """Create the string of the rectangle
         Returns:
         The string of the rectangle"""
-        return "Rectangle({:s}, {:s})". \
-            format(str(self.__width), str(self.__height))
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         """delete a instance"""
