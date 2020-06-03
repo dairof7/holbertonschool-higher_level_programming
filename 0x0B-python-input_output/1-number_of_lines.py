c35f5f7b6e47a4b0131440002b118b695ddea2ec
dairof7 <dairof7@gmail.com>
2020-06-03 18:26:07 -0500

    inicialice i

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,6 +5,7 @@
 def number_of_lines(filename=""):
     """number_of_lines Class
     Return: number if lines of a file"""
+    i = 0
     with open(filename, 'r', encoding='utf-8') as f:
         for i, line in enumerate(f):
             pass
