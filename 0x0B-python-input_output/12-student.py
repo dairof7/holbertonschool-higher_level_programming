79d5401702049ba27fd7afa07f967f5320a53872
dairof7 <dairof7@gmail.com>
2020-06-03 20:16:33 -0500

    #12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module for Student Class"""
+
+
+class Student():
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """[summary]
+
+        Args:
+            first_name ([type]): [first name]
+            last_name ([type]): [last name]
+            age ([type]): [age]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """[summary]
+
+        Returns:
+            [dict]: [dict of a class]
+        """
+        dictionary = self.__dict__
+        new_dict = {}
+        if attrs is None or attrs == []:
+            return self.__dict__
+        for key in attrs:
+            if type(key) is not str:
+                return self.__dict__
+            else:
+                if key in dictionary:
+                    new_dict[key] = dictionary[key]
+        return new_dict
