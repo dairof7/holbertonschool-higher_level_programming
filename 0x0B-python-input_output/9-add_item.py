81bab3122fb72bd454c4e55b413557cd19034fc0
dairof7 <dairof7@gmail.com>
2020-06-03 19:21:18 -0500

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -13,5 +13,4 @@ arg = sys.argv[1:]
 read_list = []
 if os.path.isfile("add_item.json"):
     read_list = load_from_json_file("add_item.json")
-    read_list += arg
-save_to_json_file(read_list, "add_item.json")
+save_to_json_file(read_list + arg, "add_item.json")
