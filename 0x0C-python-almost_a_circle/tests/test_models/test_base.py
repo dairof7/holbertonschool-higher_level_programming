73199e06cf8ce2f853d4157bb1c33b6eea4c3759
dairof7 <dairof7@gmail.com>
2020-06-09 14:43:28 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,11 @@ Unittest for Base class
 """
 
 import unittest
+import os
+import json
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class Test_Base(unittest.TestCase):
