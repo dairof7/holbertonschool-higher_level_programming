be1d624c06ecc9f0280c843fb2000342312ec330
dairof7 <dairof7@gmail.com>
2020-06-09 15:16:56 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,7 +3,7 @@
 """
 Unittest for Base class
 """
-
+import pep8
 import unittest
 import os
 import json
@@ -29,3 +29,32 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(b1.id, -7)
         self.assertEqual(b2.id, 1)
         self.assertEqual(b3.id, 2)
+
+
+class TestBasepep8(unittest.TestCase):
+    """Validate pep8"""
+
+    def test_pep8(self):
+        """test for base file and test_base file pep8"""
+        style = pep8.StyleGuide(quiet=True)
+        base = "models/base.py"
+        test_base = "tests/test_models/test_base.py"
+        result = style.check_files([base, test_base])
+        self.assertEqual(result.total_errors, 0)
+
+
+class TestDocs(unittest.TestCase):
+    """test docstrings for base and test_base files"""
+
+    def test_module(self):
+        """check module docstrings"""
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_class(self):
+        """check class docstrings"""
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_method(self):
+        """check method docstrings"""
+        for func in dir(Base):
+            self.assertTrue(len(func.__doc__) > 0)
