8cfbd7874ff4668871ffb6a84e0309dd32f1c804
dairof7 <dairof7@gmail.com>
2020-06-09 14:45:02 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -30,43 +30,5 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(b2.id, 1)
         self.assertEqual(b3.id, 2)
 
-    def test_01(self):
-        """Test - id string"""
-        b0 = Base("string")
-        self.assertEqual(b0.id, "string")
-
-    def test_02(self):
-        """Test - id none"""
-        b0 = Base(None)
-        self.assertEqual(b0.id, 1)
-
-    def test_03(self):
-        """Test - id float"""
-        b0 = Base(0.06)
-        self.assertEqual(b0.id, 0.06)
-
-    def test_04(self):
-        """Test - id boolean"""
-        b0 = Base(False)
-        self.assertEqual(b0.id, False)
-
-    def test_05(self):
-        """Test - id negative"""
-        b0 = Base(-126)
-        self.assertEqual(b0.id, -126)
-
-    def test_06(self):
-        """Test - type"""
-        b0 = Base(0.06)
-        self.assertEqual(str(type(b0)), "<class 'models.base.Base'>")
-
-    def test_07(self):
-        """Test - to_json_string"""
-        b0 = Base(4)
-        self.assertEqual(Base.to_json_string(
-            [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}]),
-            '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
-
-
 if __name__ == '__main__':
     unittest.main()
