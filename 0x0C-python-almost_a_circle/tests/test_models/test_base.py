85ca9a352fe1476e8c93dfd79d56f380886e2617
dairof7 <dairof7@gmail.com>
2020-06-09 15:39:52 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -29,3 +29,21 @@ class Test_Base(unittest.TestCase):
         self.assertEqual(b1.id, -7)
         self.assertEqual(b2.id, 1)
         self.assertEqual(b3.id, 2)
+
+    def test_load_from_file(self):
+        """test_load_from_file test classmethod output"""
+
+        Base_Base__nb_objects = 0
+        R1 = Rectangle(4, 7)
+        R2 = Rectangle(6, 9)
+        R1_d = R1.to_dictionary()
+        R2_d = R2.to_dictionary()
+        list_obj = [R1, R2]
+        Rectangle.save_to_file(list_obj)
+        list_instances = Rectangle.load_from_file()
+
+        self.assertIsInstance(list_instances[0], Rectangle)
+        self.assertIsInstance(list_instances[1], Rectangle)
+
+        self.assertDictEqual(list_instances[0].to_dictionary(), R1_d)
+        self.assertDictEqual(list_instances[1].to_dictionary(), R2_d)
