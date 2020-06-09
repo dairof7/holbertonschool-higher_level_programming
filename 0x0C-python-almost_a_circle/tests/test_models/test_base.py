5aa4b2be34e5fb0746e1ed28dee70867f5c7da1d
dairof7 <dairof7@gmail.com>
2020-06-09 14:48:03 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,16 +19,6 @@ class Test_Base(unittest.TestCase):
         """setup of unittest"""
         Base._Base__nb_objects = 0
 
-    def test_00(self):
-        """Test - id int"""
-        b0 = Base(8)
-        b1 = Base(-7)
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b0.id, 8)
-        self.assertEqual(b1.id, -7)
-        self.assertEqual(b2.id, 1)
-        self.assertEqual(b3.id, 2)
 
 if __name__ == '__main__':
     unittest.main()
