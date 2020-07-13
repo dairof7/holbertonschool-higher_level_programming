9d85a5c07a6a4222d93cc6d9b02427fa33f98699
dairof7 <dairof7@gmail.com>
2020-07-13 17:14:33 -0500

    adding files

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- this task lists all items in the table
+-- the command lists all items in the table
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- this task insert a row
+-- the command insert a row
+INSERT INTO first_table (id , name) VALUES (89, 'Holberton School');
