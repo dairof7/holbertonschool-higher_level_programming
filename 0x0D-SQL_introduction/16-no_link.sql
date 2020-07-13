5633f7cf17cdd2e3cfafe21d0494a6e0cce9d1b5
dairof7 <dairof7@gmail.com>
2020-07-13 18:43:32 -0500

    order desc

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table with a name
 -- script that lists all records of the table with a name
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
