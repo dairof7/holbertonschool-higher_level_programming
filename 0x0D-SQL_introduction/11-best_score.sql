30680282fd90307ecbeedd59aa13e3ea9cd76788
dairof7 <dairof7@gmail.com>
2020-07-13 17:57:53 -0500

    add #11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- this task list all records of table with score >= 10
+-- the command list records of a table with score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
