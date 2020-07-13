a3bc41eed50dc7521a2474e9c3b886af09d62244
dairof7 <dairof7@gmail.com>
2020-07-13 17:44:19 -0500

    fix

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- this task display number of id = 89
+-- the command display number of id = 89
+SELECT COUNT(id) FROM first_table WHERE id = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,4 @@
--- this task display number of id = 89
--- the command display number of id = 89
+-- this task create a table with data
+-- the command create a table with data
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO (1, "John", 10), (2, Alex, 3), (3, "Bob", 14), (4, "George", 8);
