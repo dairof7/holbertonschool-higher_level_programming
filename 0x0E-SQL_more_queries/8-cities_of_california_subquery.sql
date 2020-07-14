7a421c193dcaa5c2e76dcf45547588ba361e5bb6
dairof7 <dairof7@gmail.com>
2020-07-14 16:10:48 -0500

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,6 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states where name = 'California') ORDER by id ASC;
+SELECT id, name FROM cities WHERE state_id = (
+    SELECT id FROM states where name = 'California') ORDER BY id ASC;
+    
