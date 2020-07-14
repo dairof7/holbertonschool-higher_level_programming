e7008209ac44ee15661abb1dff35fb0e3f90b52e
dairof7 <dairof7@gmail.com>
2020-07-14 15:10:17 -0500

    fix basedata access

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 -- creates the database hbtn_0d_usa and the table states with id (unique, not null, primary key) and name
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
