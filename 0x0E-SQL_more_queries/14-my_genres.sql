ef487b83ff3b64c8437f002a04d3b998ff983bb1
dairof7 <dairof7@gmail.com>
2020-07-14 20:29:33 -0500

    fix uppercase

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,5 +10,5 @@ LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-where title = 'Dexter'
+WHERE title = 'Dexter'
 ORDER BY name ASC
