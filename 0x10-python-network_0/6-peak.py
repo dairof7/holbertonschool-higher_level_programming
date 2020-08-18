4704267ae2593fe5e9909f860f38962f0a54e01a
dairof7 <dairof7@gmail.com>
2020-08-18 15:02:11 -0500

    add #6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) != 0:
+        max_int = max(list_of_integers)
+    else:
+        max_int = None
+    return max_int
