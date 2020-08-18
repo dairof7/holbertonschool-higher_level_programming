0c118da5d710c3bd81e2745079cfbcb012fbddd1
dairof7 <dairof7@gmail.com>
2020-08-18 15:03:48 -0500

    add docstring

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"find the peak in a list"
 
 def find_peak(list_of_integers):
+    "find peak in a list"
     if len(list_of_integers) != 0:
         max_int = max(list_of_integers)
     else:
