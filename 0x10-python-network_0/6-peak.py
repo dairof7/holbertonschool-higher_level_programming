ca5720a89ab8598d5c57828707d6342604351735
dairof7 <dairof7@gmail.com>
2020-08-18 20:03:19 -0500

    change max to sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@
 def find_peak(list_of_integers):
     "find peak in a list"
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
         return None
