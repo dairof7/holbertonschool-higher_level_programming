653074daacc635da409d352140e3a4ed6130b88d
dairof7 <dairof7@gmail.com>
2020-09-15 12:32:50 -0500

    add sort parameters

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const array = process.argv.slice(2).sort();
+  const array = process.argv.slice(2).sort((a, b) => a - b);
   console.log(array[array.length - 2]);
 }
