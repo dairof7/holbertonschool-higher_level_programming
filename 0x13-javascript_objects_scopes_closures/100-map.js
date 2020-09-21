3e5876c40aa5d204167dfd04579fe47a34d43213
dairof7 <dairof7@gmail.com>
2020-09-21 13:33:08 -0500

    fix import

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { list } = require('./100-main');
+const { list } = require('./100-data');
 const newlist = [];
 list.map((element, index) => {
   newlist.push(element * index);
