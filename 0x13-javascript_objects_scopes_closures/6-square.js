d67304432406e9360b061622d604d113d80e0360
dairof7 <dairof7@gmail.com>
2020-09-21 12:29:16 -0500

    change c to default

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,10 +5,7 @@
 const Squaredad = require('./5-square');
 
 module.exports = class Square extends Squaredad {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
