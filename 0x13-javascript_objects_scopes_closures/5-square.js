5d109558cca879a4f3e259c3e8644669d771f85e
dairof7 <dairof7@gmail.com>
2020-09-21 12:17:24 -0500

    idk

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // Square class that inhererit from Rectangle
+const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends require('./4-rectangle') {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
