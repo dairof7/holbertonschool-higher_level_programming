337a5107c0660af563dcc9b8614dbb30d628865d
dairof7 <dairof7@gmail.com>
2020-09-21 12:13:54 -0500

    change import method

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // Square class that inhererit from Rectangle
-const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }
