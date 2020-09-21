e767328d4ba47c4c15fdeda0e36753772c71de23
dairof7 <dairof7@gmail.com>
2020-09-21 12:20:31 -0500

    style

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -12,6 +12,8 @@ module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
+    } else {
+      c = 'C';
     }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
