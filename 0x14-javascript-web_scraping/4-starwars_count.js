136116b55f667c16ba86616414758415b1c1bebb
dairof7 <dairof7@gmail.com>
2020-09-22 14:58:28 -0500

    change compare

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,11 @@
 const request = require('request');
 const url = process.argv[2];
 const char = 'https://swapi-api.hbtn.io/api/people/18/';
-let count = 0;
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
+    let count = 0;
     const data = JSON.parse(body);
     for (let i = 0; i < data.count; i++) {
       for (let j = 0; j < data.results[i].characters.length; j++) {
