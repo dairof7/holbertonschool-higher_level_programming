0c8419f45d0723965ffa17db0b12227cd3ff5496
dairof7 <dairof7@gmail.com>
2020-09-22 14:34:06 -0500

    add #4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const char = 'https://swapi-api.hbtn.io/api/people/18/';
+let count = 0;
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.count; i++) {
+      for (let j = 0; j < data.results[i].characters.length; j++) {
+        if (char === data.results[i].characters[j]) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
