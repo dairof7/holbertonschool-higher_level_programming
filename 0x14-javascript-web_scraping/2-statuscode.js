86805f6670e82517c9b2b3de6c7eb661547bcd00
dairof7 <dairof7@gmail.com>
2020-09-22 12:23:22 -0500

    style

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const request = require('request');
 
 request(process.argv[2], function (err, data) {
-  if (err){
-    console.log(err)
+  if (err) {
+    console.log(err);
   } else {
     console.log('code:', data.statusCode);
   }
