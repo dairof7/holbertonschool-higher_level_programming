5818691338a5a6a473d27a07107d6432d8d132ed
dairof7 <dairof7@gmail.com>
2020-09-22 14:50:11 -0500

    change compare

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const char = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 request(url, function (err, res, body) {
   if (err) {
@@ -10,7 +9,7 @@ request(url, function (err, res, body) {
     const data = JSON.parse(body);
     for (let i = 0; i < data.count; i++) {
       for (let j = 0; j < data.results[i].characters.length; j++) {
-        if (char === data.results[i].characters[j]) {
+        if (data.results[i].characters[j].includes('/18/')) {
           count += 1;
           break;
         }
